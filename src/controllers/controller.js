a29c9e6d2c2830c27c366f664e21a201ab14cece
keerti96 <savedrive16@gmail.com>
2022-06-29 23:36:48 +0530

    create college API

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -11,6 +11,12 @@ const isValidRequestBody = function (requestBody){
     return Object.keys(requestBody).length > 0;
 }
 
+const isValid = function(value){
+    if(typeof value === 'undefined' || value === null) return false;
+    if(typeof value === 'string' && value.trim().length === 0) return false;
+    return true;    
+} 
+
 
 const createCollege = async function (req, res) {
     try{
@@ -19,9 +25,31 @@ const createCollege = async function (req, res) {
         console.log(requestBody)
 
         if (!isValidRequestBody(requestBody)){
-            return res.status(400).send({ status: false, msg: "Request body is empty!! Please provide the college details" })
+            return res.status(400).send({ status: false, message: "Request body is empty!! Please provide the college details" })
         }
 
+        const {name, fullName, logoLink} = requestBody;
+
+        if(!isValid(name)){
+            return res.status(400).send({ status: false, message: "Please provide the name of college" })
+        }
+
+        if(!isValid(fullName)){
+            return res.status(400).send({ status: false, message: "Please provide the fullName of college" })
+        }
+
+        if(!isValid(logoLink)){
+            return res.status(400).send({ status: false, message: "Please provide the logoLink of college" })
+        }
+
+        //----check if name is unique
+        const nameCheck=await collegeModel.find({name:name})
+        if(nameCheck.length!=0){
+            return res.status(400).send({ status: false, message: "this name(abbr) already exists" })
+        }
+
+
+        //<----create a college document---->
         const savedData = await collegeModel.create(requestBody)
         res.status(201).send({ status: true, data: savedData })
     }
