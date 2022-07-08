9720f09edd8822161dda65eb51b1c3af6c5b48f8
savedrive@gmail.com <savedrive@gmail.com>
2022-07-08 18:09:14 +0530

    project completed

--- src/validator/reviewvalidation.js
+++ src/validator/reviewvalidation.js
@@ -1,53 +1,65 @@
-const { isValidRequestBody, isValidData, isValidObjectId } = require("../validator/validation")
-
-
-
-const ratingCheck = function (value) {
-   num = /^[1-5]{1}$/
-   if (!num.test(value)) return false
-
-   return true
-}
-
+const { isValidRequestBody, isValidObjectId, isValidData } = require("../validator/validation")
 
 
 const reviewCheck = function (req, res, next) {
-   const requestBody = req.body
-   bookId = req.params
-   if (!isValidRequestBody(requestBody))
-      return res.status(400).send({ status: false, message: "please give review and details" })
+    try {
+
+        const requestBody = req.body
+
+        if (!isValidRequestBody(requestBody)) {
+            return res.status(400).send({ status: false, message: "Request body is empty!! Please provide the college details" })
+        }
+
+        const { bookId, reviewedBy, reviewedAt, rating, review } = requestBody;
+
+        //check if each mandatory field is present in request body
+        let invalidParam = "";
+
+        // if (!isValidObjectId(bookId)) {
+        //     invalidParam = "bookId"
+        // }
+        if (!isValidData(reviewedBy)) {
+            invalidParam = invalidParam + "reviewedBy"
+
+        }
+        if (!isValidData(reviewedAt)) {
+            invalidParam = invalidParam + " " + "reviewedAt"
+
+        }
+        if (!rating || typeof rating !=  || rating < 1 || rating > 5) {
+            invalidParam = invalidParam + "rating"
+        }
 
 
-   const { reviewedBy, rating, review } = requestBody
-   // =========check wheather mandatory  fields are present or not=======================//
-   let missdata = ""
-   if (!bookId) {
-      missdata = missdata + "bookId"
-   }
+        if (invalidParam) {
+            let message = invalidParam + " : invalid input parameters "
+            return res.status(400).send({ status: false, msg: message })
+        }
 
-   if (!ratingCheck(rating)) {
-      missdata = missdata + " " + "rating"
-   }
+        //validating other constraints
+        // if (!isValidObjectId(bookId)) {
+        //     return res.status(400).send({ status: false, msg: "Invalid bookId" })
+        // }
+
+        // if(rating<1 || rating > 5){
+        //     return res.status(400).send({ status: false, msg: "Rating should be 1 to 5" })
+        // }
+
+        //review is optional parameter, but if provided it should be a non-empty string
+        if (review) {
+            if (!isValidData(review))
+                return res.status(400).send({ status: false, msg: "Review type must be string and can't provide empty string" })
+        }
 
 
-   if (missdata) {
-      let message = missdata + " " + "is missing or not in proper format"
-      return res.status(400).send({ status: false, message: message })
-   }
-   if (reviewedBy) {
-      if (!isValidData(reviewedBy))
-         return res.status(400).send({ status: false, message: "give proper name as reviewdBy" })
-   }
+        //if all validations are correct then go to controller
+        next()
 
-   if (!isValidObjectId(bookId.bookId)) {
-      return res.status(400).send({ status: false, message: "not a valid bookId" })
-   }
+    }
+    catch (err) {
+        res.status(500).send({ status: false, error: err.message })
+    }
 
-   next()
 }
 
-
-
-
-
-module.exports = { reviewCheck }
+module.exports = {reviewCheck}
